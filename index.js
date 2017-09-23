be35002b9cdf631676863cff9384d93b98c31213
manjrekarom <manjrekarom@gmail.com>
2017-09-23 15:51:01 +0530

    Added environment variable PORT.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.set('view engine','hbs');
 hbs.registerPartials(__dirname+'/views/partials/');
 
